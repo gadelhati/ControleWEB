60b2620ffb64d912959d73bcf77551a70aa5f459
Gadelha TI <gadelha.ti@gmail.com>
2016-03-04 11:24:01 -0300

    1123 04032016

---
+++ WebContent/Filtro/js/script.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+    var macAddress = "";
+    var ipAddress = "";
+    var computerName = "";
+    var wmi = GetObject("winmgmts:{impersonationLevel=impersonate}");
+    e = new Enumerator(wmi.ExecQuery("SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = True"));
+    for(; !e.atEnd(); e.moveNext()) {
+        var s = e.item(); 
+        macAddress = s.MACAddress;
+        ipAddress = s.IPAddress(0);
+        computerName = s.DNSHostName;
+    } 
+</script>
